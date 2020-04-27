122abb5dac1bdce16874785a073513594533463c
Chang Wang <garnwraly@gmail.com>
2020-04-27 14:59:34 -0400

    use cache and link from @apollo/client

--- src/index.js
+++ src/index.js
@@ -1,22 +1,20 @@
 import React from "react";
 import { render } from "react-dom";
 import { ApolloProvider } from "@apollo/react-hooks";
-import { ApolloClient } from "@apollo/client";
-import { InMemoryCache } from "apollo-cache-inmemory";
-import { HttpLink } from "apollo-link-http";
+import { ApolloClient, HttpLink, InMemoryCache } from "@apollo/client";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import App from "./App";
 
 const API_URL = "https://api.graph.cool/simple/v1/ciybssqs700el0132puboqa9b";
 
-const cache = new InMemoryCache();
+// const cache = new InMemoryCache();
 const link = new HttpLink({
   uri: API_URL
 });
 
 const client = new ApolloClient({
   link,
-  cache,
+  cache: new InMemoryCache(),
   queryDeduplication: true
 });
 
